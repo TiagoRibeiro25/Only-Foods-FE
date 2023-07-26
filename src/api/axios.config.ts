ff138ecee71b41c87e00f807df8b663cacd4ec09
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-26 16:39:36 +0100

    refactor: Updated axios config to support cookies

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -5,6 +5,7 @@ const axiosOptions: CreateAxiosDefaults = {
 	headers: { 'Content-Type': 'application/json' }, // Default header for all requests
 	timeout: 60000, // 60 seconds
 	timeoutErrorMessage: 'Request timed out', // Error message when request times out
+	withCredentials: true, // Enable sending and receiving cookies from the server
 };
 
 const api: AxiosInstance = axios.create(axiosOptions);
