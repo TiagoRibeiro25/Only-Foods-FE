740aab66a3193a38e6d5fa7915079c4513352a10
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 21:12:49 +0100

    feat: Add a custom phrase to the loading view
    Every 5 seconds, the phrase will change to a different one.
    All phrases are in the phrases.ts file

--- src/views/Loading/index.tsx
+++ src/views/Loading/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import Logo from '../../assets/logo/logo_color_2.png';
+import phrases from './phrases';
 
 interface LoadingProps {
 	state: 'loading' | 'error';
 }
 
 const Loading = (props: LoadingProps) => {
+	const [currentPhrase, setCurrentPhrase] = useState(
+		phrases[Math.floor(Math.random() * phrases.length)],
+	);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentPhrase(phrases[Math.floor(Math.random() * phrases.length)]);
+		}, 5000); // Change the phrase 5 seconds
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
 	return (
 		<div>
 			<div className="absolute z-10 flex items-center justify-center w-full h-full bg-white bg-opacity-60">
@@ -24,31 +40,34 @@ const Loading = (props: LoadingProps) => {
 							</span>
 						</div>
 					) : (
-						<div className="flex flex-row mt-2">
-							<span className="mr-4 text-2xl select-none font-bellefair">Loading</span>
-							<div className="flex items-center">
-								<svg
-									className="w-6 h-6 text-gray-800 animate-spin"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-								>
-									<circle
-										className="opacity-25"
-										cx="12"
-										cy="12"
-										r="10"
-										stroke="currentColor"
-										strokeWidth="4"
-									></circle>
-									<path
-										className="opacity-75"
-										fill="currentColor"
-										d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-									></path>
-								</svg>
+						<>
+							<div className="flex flex-row mt-2">
+								<span className="mr-4 text-2xl select-none font-bellefair">Loading</span>
+								<div className="flex items-center">
+									<svg
+										className="w-6 h-6 text-gray-800 animate-spin"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+									>
+										<circle
+											className="opacity-25"
+											cx="12"
+											cy="12"
+											r="10"
+											stroke="currentColor"
+											strokeWidth="4"
+										></circle>
+										<path
+											className="opacity-75"
+											fill="currentColor"
+											d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+										></path>
+									</svg>
+								</div>
 							</div>
-						</div>
+							<span className="mt-5 text-xl font-bellefair">{currentPhrase}</span>
+						</>
 					)}
 				</div>
 			</div>
