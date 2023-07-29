6bd2877d4bcdbda1d986f067382dd40a68900d7d
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 18:18:11 +0100

    feat: Check if user is logged on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import requests from './api/requests';
+import { UserContext } from './contextProviders/user.context';
 import Navbar from './layouts/Navbar';
 import Navigation from './navigation';
+import Loading from './views/Loading';
 
 function App() {
+	const { setLoggedUser } = useContext(UserContext);
+	const [loading, setLoading] = useState(true);
+
+	// Function to fetch logged user data
+	const fetchLoggedUser = useCallback(async () => {
+		try {
+			const response = await requests.users.getLoggedUser();
+
+			const user = response.data.data && {
+				id: response.data.data.id,
+				username: response.data.data.username,
+				email: response.data.data.email,
+				isAdmin: response.data.data.isAdmin,
+				picture: response.data.data.userImage?.cloudinaryImage ?? undefined,
+			};
+
+			setLoggedUser(user ?? null);
+		} catch (error) {
+			console.log(error);
+			setLoggedUser(null);
+		}
+
+		setLoading(false);
+	}, [setLoggedUser]);
+
+	// Check if the user is logged in by sending a request to the server
+	useEffect(() => {
+		fetchLoggedUser();
+	}, [fetchLoggedUser]);
+
 	return (
-		<Router>
-			<header>
-				<Navbar />
-			</header>
-			<main className="max-w-screen-xl px-4 mx-auto mt-20">
-				<Navigation />
-			</main>
-		</Router>
+		<>
+			{loading ? (
+				<Loading />
+			) : (
+				<Router>
+					<header>
+						<Navbar />
+					</header>
+					<main className="max-w-screen-xl px-4 mx-auto mt-20">
+						<Navigation />
+					</main>
+				</Router>
+			)}
+		</>
 	);
 }
 
