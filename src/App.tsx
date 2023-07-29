8c0db0ce50526ceb07ecf25a52dfd0dad925d875
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 15:03:23 +0100

    revert: Removed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './layouts/Navbar';
 import Navigation from './navigation';
-import store from './redux/index';
 
 function App() {
 	return (
-		<Provider store={store}>
-			<Router>
-				<header>
-					<Navbar />
-				</header>
-				<main className="max-w-screen-xl px-4 mx-auto mt-20">
-					<Navigation />
-				</main>
-			</Router>
-		</Provider>
+		<Router>
+			<header>
+				<Navbar />
+			</header>
+			<main className="max-w-screen-xl px-4 mx-auto mt-20">
+				<Navigation />
+			</main>
+		</Router>
 	);
 }
 
