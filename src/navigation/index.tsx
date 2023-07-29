34a027dde979f9ef516c10e63fdf373f51515aa2
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 18:21:20 +0100

    feat: Feed view file (init)

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { UserContext } from '../contextProviders/user.context';
+import Feed from '../views/Feed';
 import Home from '../views/Home';
 import NotFound from '../views/NotFound';
 import ResetPassword from '../views/ResetPassword';
@@ -15,6 +16,7 @@ const Navigation = () => {
 				element={!loggedUser ? <Home /> : <Navigate to="/feed" replace />}
 			/>
 			<Route path="/reset-password/:token" element={<ResetPassword />} />
+			<Route path="/feed" element={loggedUser ? <Feed /> : <Navigate to="/" replace />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
