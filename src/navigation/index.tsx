5a11e25a2df014e160e401581049e17036b2877d
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 18:21:02 +0100

    feat: Guard Routes

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { UserContext } from '../contextProviders/user.context';
 import Home from '../views/Home';
 import NotFound from '../views/NotFound';
 import ResetPassword from '../views/ResetPassword';
 
 const Navigation = () => {
+	const { loggedUser } = useContext(UserContext);
+
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/reset-password/:token" element={<ResetPassword />} />'
+			<Route
+				path="/"
+				element={!loggedUser ? <Home /> : <Navigate to="/feed" replace />}
+			/>
+			<Route path="/reset-password/:token" element={<ResetPassword />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
