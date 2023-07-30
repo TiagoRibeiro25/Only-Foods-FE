378f75e883d55fd627e1bb25637ff9a0f29c099a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-30 01:05:43 +0100

    Feat: Add new tought structure
    TODO: Missing API call

--- src/views/Feed/index.tsx
+++ src/views/Feed/index.tsx
@@ -1,5 +1,53 @@
+import { useState } from 'react';
+import PostTextArea from '../../components/PostTextarea';
+import Reveal from '../../components/Reveal';
+
 const Feed = () => {
-	return <div>Feed</div>;
+	const [newThoughtText, setNewThoughtText] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const [statusMsg, setStatusMsg] = useState('Error Message');
+
+	const handleNewPost = (): void => {
+		console.log(newThoughtText);
+		setLoading(true);
+		setStatusMsg('Error Message');
+
+		// Simulate a delay
+		//TODO: Remove this
+		setTimeout(() => {
+			setLoading(false);
+			setStatusMsg('Success Message');
+			setNewThoughtText('');
+		}, 2000);
+	};
+
+	return (
+		<div className="flex flex-col items-center max-w-3xl mx-auto mt-28">
+			<Reveal width="100%" animation="slide-top" delay={0.05}>
+				{/* Add New Thought Form */}
+				<PostTextArea
+					id="new-thought-textarea"
+					labelText="New Thought"
+					placeholder="Share your current thought with the world..."
+					maxLength={1000}
+					value={newThoughtText}
+					loading={loading}
+					onChange={setNewThoughtText}
+					onSubmit={handleNewPost}
+				/>
+
+				{/* Status Message */}
+				<div className="w-full my-3 text-center">
+					<p
+						className="text-sm text-gray-950"
+						style={{ visibility: statusMsg === 'Error Message' ? 'hidden' : 'visible' }}
+					>
+						{statusMsg}
+					</p>
+				</div>
+			</Reveal>
+		</div>
+	);
 };
 
 export default Feed;
