cce39429926b97daa4670c4f6790144947efb569
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-30 23:49:48 +0100

    feat: Select Component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,54 @@
+interface Option {
+	text: string;
+	value: string;
+}
+
+interface SelectProps {
+	id: string;
+	labelText: string;
+	value: string;
+	options: Option[];
+	onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const Select = (props: SelectProps) => {
+	return (
+		<>
+			<label htmlFor={props.id} className="sr-only">
+				{props.labelText}
+			</label>
+			<div className="relative">
+				<select
+					id={props.id}
+					className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200"
+					defaultValue={props.value}
+					onChange={props.onChange}
+				>
+					{props.options.map(option => (
+						<option key={option.value} value={option.value}>
+							{option.text}
+						</option>
+					))}
+				</select>
+				<div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 pointer-events-none">
+					<svg
+						className="w-4 h-4"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M19 9l-7 7-7-7"
+						></path>
+					</svg>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Select;
--- src/views/Feed/index.tsx
+++ src/views/Feed/index.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import Reveal from '../../components/Reveal';
+import Select from '../../components/Select';
 import NewThoughtForm from './components/NewThoughtForm';
 
+type Filter = 'recent' | 'popular' | 'following';
+
+const options = [
+	{ text: 'Recent', value: 'recent' },
+	{ text: 'Most Popular', value: 'popular' },
+	{ text: 'Following', value: 'following' },
+];
+
 const Feed = () => {
+	const [filter, setFilter] = useState<Filter>('recent');
+
 	return (
 		<div className="flex flex-col items-center max-w-3xl mx-auto mt-28">
 			<Reveal width="100%" animation="slide-top" delay={0.05}>
 				<NewThoughtForm onSubmit={() => console.log('New Thought Submitted!')} />
+			</Reveal>
+
+			{/* Filter Thoughts */}
+			<Reveal width="100%" animation="slide-right" delay={0.05}>
+				<div className="flex justify-end w-full mt-6">
+					<div className="w-40">
+						<Select
+							id="filter-thoughts"
+							labelText="Filter Thoughts"
+							options={options}
+							value={filter}
+							onChange={e => setFilter(e.target.value as Filter)}
+						/>
+					</div>
+				</div>
 			</Reveal>
 		</div>
 	);
