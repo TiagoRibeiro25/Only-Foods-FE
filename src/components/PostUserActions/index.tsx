2f5ec6fae3a2f145607f13dcc4659a27d5a8ab70
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-03 17:21:38 +0100

    feat: User Actions Component
    Component that let's the user copy the url of the thought/recipe, like/unlike it and go to the item view to see the comments

---
+++ src/components/PostUserActions/index.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from 'react-router-dom';
+import requests from '../../api/requests';
+import CommentIcon from '../../assets/icons/comment.svg';
+import CopiedIcon from '../../assets/icons/copied.svg';
+import CopyIcon from '../../assets/icons/copy.svg';
+import LikeIcon from '../../assets/icons/like.svg';
+import LikedIcon from '../../assets/icons/liked.svg';
+import LoadingIcon from '../../assets/icons/loading.svg';
+import formatData from '../../utils/formatData';
+
+interface PostUserActionsProps {
+	type: 'thought' | 'recipe';
+	id: number;
+	likes: number;
+	comments: number;
+	isAuthor: boolean;
+	isLiked: boolean;
+	onLikeUpdate: (id: number, newLikes: number, isLiked: boolean) => void;
+}
+
+const PostUserActions = (props: PostUserActionsProps) => {
+	const [isCopied, setIsCopied] = useState<boolean>(false);
+	const [isLiked, setIsLiked] = useState<boolean>(props.isLiked);
+	const [liking, setLiking] = useState<boolean>(false);
+	const [likeIcon, setLikeIcon] = useState<string>(LikeIcon);
+
+	const handleLike = async (): Promise<void> => {
+		if (liking) return;
+
+		setLiking(true);
+
+		try {
+			const response = await requests.thoughts.handleLike(props.id);
+
+			if (response.data.success) {
+				const newLikeStatus: boolean = !isLiked;
+				setIsLiked(newLikeStatus);
+
+				// Update the number of likes in the thoughts state
+				const newLikes: number = isLiked ? props.likes - 1 : props.likes + 1;
+				props.onLikeUpdate(props.id, newLikes, newLikeStatus);
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLiking(false);
+		}
+	};
+
+	const handleCopy = (): void => {
+		if (!isCopied) {
+			setIsCopied(true);
+			navigator.clipboard.writeText(
+				`${window.location.origin}/${props.type}/${props.id}`,
+			);
+
+			setTimeout(() => {
+				setIsCopied(false);
+			}, 700);
+		}
+	};
+
+	useEffect(() => {
+		if (liking) {
+			setLikeIcon(LoadingIcon);
+		} else {
+			setLikeIcon(isLiked ? LikedIcon : LikeIcon);
+		}
+	}, [isLiked, liking]);
+
+	return (
+		<>
+			{/* Copy Link */}
+			<div className="flex items-center">
+				<LazyLoadImage
+					src={isCopied ? CopiedIcon : CopyIcon}
+					alt={'Copy' + props.type + 'Link'}
+					effect="opacity"
+					className="cursor-pointer hover:opacity-80"
+					width={20}
+					onClick={handleCopy}
+				/>
+			</div>
+
+			{/* Likes */}
+			<div className="flex items-center">
+				<LazyLoadImage
+					src={likeIcon}
+					alt={'Like' + props.type}
+					effect="opacity"
+					className={`cursor-pointer hover:opacity-80 ${liking ? 'animate-spin' : ''}`}
+					width={22}
+					onClick={handleLike}
+				/>
+
+				{/* Like Count */}
+				<span className="ml-1 text-sm font-semibold">
+					{formatData.likes(props.likes)}
+				</span>
+			</div>
+
+			{/* Comments */}
+			<div className="flex items-center">
+				<Link to={`/${props.type}/${props.id}`} className="flex items-center">
+					<LazyLoadImage
+						src={CommentIcon}
+						alt="See Comments"
+						effect="opacity"
+						className="cursor-pointer hover:opacity-80"
+						width={22}
+					/>
+				</Link>
+
+				{/* Like Count */}
+				<span className="ml-1 text-sm font-semibold">
+					{formatData.comments(props.comments)}
+				</span>
+			</div>
+		</>
+	);
+};
+
+export default PostUserActions;
