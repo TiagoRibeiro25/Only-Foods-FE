84d802946ffc231cf33d80d91566d5ee3daf0f0e
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-05 00:35:13 +0100

    refactor: Made renderParagraphs a global component

---
+++ src/components/HTMLText/index.tsx
@@ -0,0 +1,67 @@
+interface HTMLTextProps {
+	text: string;
+}
+
+// Regular expression to identify links in the paragraph and capture the link text and URL
+const linkRegex = /(?:(?:https?|ftp):\/\/|www\.)[^\s/$.?#].[^\s]*/gi;
+
+const HTMLText = (props: HTMLTextProps) => {
+	const paragraphs = props.text.split('\n'); // Split content into paragraphs
+
+	const renderParagraphWithLinks = (paragraph: string): React.JSX.Element => {
+		const matches = paragraph.match(linkRegex);
+
+		// if the link doesn't have http or https, add http to it
+		// if it has www replace it with http
+		const newMatches = matches?.map(match => {
+			if (match.startsWith('www')) {
+				return `http://${match}`;
+			} else {
+				return match;
+			}
+		});
+
+		// If there are links in the paragraph, replace them with anchor tags
+		if (newMatches && newMatches.length > 0) {
+			return (
+				<p key={crypto.randomUUID()} className="text-md">
+					{paragraph.split(linkRegex).map((part, index) => {
+						if (index % 2 === 0) {
+							return part; // Non-link text
+						} else {
+							// Link text and URL
+							return (
+								<a
+									key={crypto.randomUUID()}
+									href={newMatches[index - 1]}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-blue-500 hover:underline"
+								>
+									{newMatches[index - 1].replace(/(^\w+:|^)\/\//, '')}
+								</a>
+							);
+						}
+					})}
+				</p>
+			);
+		} else {
+			// If there are no links in the paragraph, return the paragraph as it is
+			return (
+				<p key={crypto.randomUUID()} className="text-md">
+					{paragraph}
+				</p>
+			);
+		}
+	};
+
+	return paragraphs.map(paragraph => {
+		if (paragraph.trim() === '') {
+			return <br key={crypto.randomUUID()} />;
+		} else {
+			return renderParagraphWithLinks(paragraph);
+		}
+	});
+};
+
+export default HTMLText;
--- src/components/Thought/index.tsx
+++ src/components/Thought/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Link } from 'react-router-dom';
 import requests from '../../api/requests';
@@ -8,6 +8,7 @@ import EditIcon from '../../assets/icons/edit.svg';
 import LoadingIcon from '../../assets/icons/loading.svg';
 import UserPlaceholderPicture from '../../assets/imgs/user.png';
 import ConfirmActionModal from '../ConfirmActionModal';
+import HTMLText from '../HTMLText';
 import PostTextArea from '../PostTextarea';
 import PostUserActions from '../PostUserActions';
 
@@ -37,65 +38,12 @@ interface Props {
 	onLikeUpdate: (id: number, newLikes: number, isLiked: boolean) => void;
 }
 
-// Regular expression to identify links in the paragraph and capture the link text and URL
-const linkRegex = /(?:(?:https?|ftp):\/\/|www\.)[^\s/$.?#].[^\s]*/gi;
-
 const Thought = (props: Props) => {
 	const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
 	const [deletingPost, setDeletingPost] = useState<boolean>(false);
 	const [editModeEnabled, setEditModeEnabled] = useState<boolean>(false);
 	const [editingPost, setEditingPost] = useState<boolean>(false);
 	const [editedPost, setEditedPost] = useState<string>('');
-
-	const paragraphs = props.thought.content.split('\n'); // Split content into paragraphs
-
-	// Function to replace links with anchor tags
-	const renderParagraphWithLinks = (paragraph: string): React.JSX.Element => {
-		const matches = paragraph.match(linkRegex);
-
-		// if the link doesn't have http or https, add http to it
-		// if it has www replace it with http
-		const newMatches = matches?.map(match => {
-			if (match.startsWith('www')) {
-				return `http://${match}`;
-			} else {
-				return match;
-			}
-		});
-
-		// If there are links in the paragraph, replace them with anchor tags
-		if (newMatches && newMatches.length > 0) {
-			return (
-				<p key={crypto.randomUUID()} className="text-md">
-					{paragraph.split(linkRegex).map((part, index) => {
-						if (index % 2 === 0) {
-							return part; // Non-link text
-						} else {
-							// Link text and URL
-							return (
-								<a
-									key={crypto.randomUUID()}
-									href={newMatches[index - 1]}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="text-blue-500 hover:underline"
-								>
-									{newMatches[index - 1].replace(/(^\w+:|^)\/\//, '')}
-								</a>
-							);
-						}
-					})}
-				</p>
-			);
-		} else {
-			// If there are no links in the paragraph, return the paragraph as it is
-			return (
-				<p key={crypto.randomUUID()} className="text-md">
-					{paragraph}
-				</p>
-			);
-		}
-	};
 
 	// Function to delete a post
 	const deletePost = async (): Promise<void> => {
@@ -222,13 +170,7 @@ const Thought = (props: Props) => {
 			{/* Second Row */}
 			<div className="my-4">
 				{!editModeEnabled ? (
-					paragraphs.map(paragraph => {
-						if (paragraph.trim() === '') {
-							return <br key={crypto.randomUUID()} />;
-						} else {
-							return renderParagraphWithLinks(paragraph);
-						}
-					})
+					<HTMLText text={props.thought.content} />
 				) : (
 					<PostTextArea
 						id={`post-${props.thought.id}-edit`}
