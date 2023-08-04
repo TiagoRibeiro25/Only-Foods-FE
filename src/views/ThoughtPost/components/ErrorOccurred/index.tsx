e06e7d1a11cd2164ef8d90c3eae0184bc2008749
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-04 23:29:09 +0100

    feat: Thought Post comments view

---
+++ src/views/ThoughtPost/components/ErrorOccurred/index.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../../../components/Button';
+import Reveal from '../../../../components/Reveal';
+
+const ErrorOccurred = () => {
+	const navigate = useNavigate();
+	const goBack = () => navigate(-1);
+
+	return (
+		<Reveal width="100%" animation="slide-bottom" delay={0.3} duration={0.7}>
+			<div className="flex flex-col items-center justify-center gap-2 pt-8 pb-12 text-center col-span-full md:pb-6">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="#878d98"
+					className="w-10 h-10"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+					/>
+				</svg>
+
+				<p className="mb-3 text-xl text-center text-gray-500">
+					Something went wrong. Please try again later.
+				</p>
+				<Button type="button" padding="0.5rem 2rem" onClick={goBack}>
+					Go back
+				</Button>
+			</div>
+		</Reveal>
+	);
+};
+
+export default ErrorOccurred;
--- src/views/ThoughtPost/index.tsx
+++ src/views/ThoughtPost/index.tsx
@@ -1,12 +1,12 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import requests from '../../api/requests';
-import Button from '../../components/Button';
 import Loading from '../../components/Loading';
 import Post from '../../components/Post';
 import Reveal from '../../components/Reveal';
 import { ThoughtsContext } from '../../contextProviders/ThoughtsContext';
 import { UserContext } from '../../contextProviders/UserContext';
+import ErrorOccurred from './components/ErrorOccurred';
 
 interface Thought {
 	id: number;
@@ -34,11 +34,8 @@ const ThoughtPost = () => {
 	const thoughtsContext = useContext(ThoughtsContext);
 
 	const [thought, setThought] = useState<Thought>();
-	const [isLoading, setIsLoading] = useState(true);
-
-	const goBack = () => {
-		navigate(-1);
-	};
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [errorOccurred, setErrorOccurred] = useState<boolean>(false);
 
 	const fetchThought = useCallback(async () => {
 		try {
@@ -58,6 +55,7 @@ const ThoughtPost = () => {
 			}
 		} catch (error) {
 			console.log(error);
+			setErrorOccurred(true);
 		} finally {
 			setIsLoading(false);
 		}
@@ -73,7 +71,7 @@ const ThoughtPost = () => {
 			{/* Thought */}
 			<div className="w-full mt-14">
 				{isLoading && <Loading />}
-				{thought && !isLoading ? (
+				{thought && !isLoading && (
 					<Reveal width="100%" animation="slide-right" delay={0.05}>
 						<Post
 							isAdmin={loggedUser?.isAdmin ?? false}
@@ -93,35 +91,8 @@ const ThoughtPost = () => {
 							}}
 						/>
 					</Reveal>
-				) : (
-					<div className="flex flex-col justify-center h-96">
-						<Reveal width="100%" animation="slide-bottom" delay={0.3} duration={0.7}>
-							<div className="flex flex-col items-center justify-center gap-2 pt-8 pb-12 text-center col-span-full md:pb-6">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="#878d98"
-									className="w-10 h-10"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-									/>
-								</svg>
-
-								<p className="mb-3 text-xl text-center text-gray-500">
-									Something went wrong. Please try again later.
-								</p>
-								<Button type="button" padding="0.5rem 2rem" onClick={goBack}>
-									Go back
-								</Button>
-							</div>
-						</Reveal>
-					</div>
 				)}
+				{errorOccurred && <ErrorOccurred />}
 			</div>
 
 			{/* Comments */}
