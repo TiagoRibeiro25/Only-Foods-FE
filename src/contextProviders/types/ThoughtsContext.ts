621474bc81b2f0a6fc7e59385d25e0e5479c2b17
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-06 15:23:41 +0100

    refactor: Improved types

--- src/contextProviders/types/ThoughtsContext.ts
+++ src/contextProviders/types/ThoughtsContext.ts
@@ -1,36 +1,20 @@
 import { IThought } from '../../types/types';
 
+interface ThoughtsState {
+	thoughts: IThought[];
+	setThoughts: (thoughts: IThought[]) => void;
+	page: number;
+	incrementPage: () => void;
+	reachedEnd: boolean;
+	setReachedEnd: (reachedEnd: boolean) => void;
+	isInitialLoad: boolean;
+	setIsInitialLoad: (isInitialLoad: boolean) => void;
+}
+
 export interface ThoughtsContextType {
-	recent: {
-		thoughts: IThought[];
-		setThoughts: (thoughts: IThought[]) => void;
-		page: number;
-		incrementPage: () => void;
-		reachedEnd: boolean;
-		setReachedEnd: (reachedEnd: boolean) => void;
-		isInitialLoad: boolean;
-		setIsInitialLoad: (isInitialLoad: boolean) => void;
-	};
-	popular: {
-		thoughts: IThought[];
-		setThoughts: (thoughts: IThought[]) => void;
-		page: number;
-		incrementPage: () => void;
-		reachedEnd: boolean;
-		setReachedEnd: (reachedEnd: boolean) => void;
-		isInitialLoad: boolean;
-		setIsInitialLoad: (isInitialLoad: boolean) => void;
-	};
-	following: {
-		thoughts: IThought[];
-		setThoughts: (thoughts: IThought[]) => void;
-		page: number;
-		incrementPage: () => void;
-		reachedEnd: boolean;
-		setReachedEnd: (reachedEnd: boolean) => void;
-		isInitialLoad: boolean;
-		setIsInitialLoad: (isInitialLoad: boolean) => void;
-	};
+	recent: ThoughtsState;
+	popular: ThoughtsState;
+	following: ThoughtsState;
 	resetAllState: () => void;
 }
 
