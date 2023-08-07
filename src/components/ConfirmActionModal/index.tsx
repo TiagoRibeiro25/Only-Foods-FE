4869fa079c73fc580cd1afa3209679a75c276c9c
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-07 19:32:17 +0100

    refactor: Modal fade animation

--- src/components/ConfirmActionModal/index.tsx
+++ src/components/ConfirmActionModal/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Reveal from '../Reveal';
 import { ConfirmActionModalProps } from './types';
 
 const ConfirmActionModal = (props: ConfirmActionModalProps) => {
@@ -17,63 +18,65 @@ const ConfirmActionModal = (props: ConfirmActionModalProps) => {
 					props.show ? 'block' : 'hidden'
 				}`}
 			>
-				<div className="relative bg-white rounded-lg shadow">
-					<button
-						type="button"
-						className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
-						onClick={props.onCancel}
-					>
-						<svg
-							className="w-3 h-3"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 14 14"
-						>
-							<path
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-							/>
-						</svg>
-						<span className="sr-only">Close modal</span>
-					</button>
-					<div className="p-6 text-center">
-						<svg
-							className="w-12 h-12 mx-auto mb-4 text-gray-400"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 20 20"
-						>
-							<path
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-							/>
-						</svg>
-						<h3 className="mb-5 text-lg font-normal text-gray-500">{props.message}</h3>
+				<Reveal animation="fade" delay={0.05} duration={0.4}>
+					<div className="relative bg-white rounded-lg shadow">
 						<button
 							type="button"
-							className="text-white bg-gray-900 hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-gray-900 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-							onClick={props.onConfirm}
-						>
-							{props.confirmText ?? "Yes, I'm sure"}
-						</button>
-
-						<button
-							type="button"
-							className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+							className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
 							onClick={props.onCancel}
 						>
-							{props.cancelText ?? 'No, cancel'}
+							<svg
+								className="w-3 h-3"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+								/>
+							</svg>
+							<span className="sr-only">Close modal</span>
 						</button>
+						<div className="p-6 text-center">
+							<svg
+								className="w-12 h-12 mx-auto mb-4 text-gray-400"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 20 20"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+								/>
+							</svg>
+							<h3 className="mb-5 text-lg font-normal text-gray-500">{props.message}</h3>
+							<button
+								type="button"
+								className="text-white bg-gray-900 hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-gray-900 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+								onClick={props.onConfirm}
+							>
+								{props.confirmText ?? "Yes, I'm sure"}
+							</button>
+
+							<button
+								type="button"
+								className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+								onClick={props.onCancel}
+							>
+								{props.cancelText ?? 'No, cancel'}
+							</button>
+						</div>
 					</div>
-				</div>
+				</Reveal>
 			</div>,
 			document.body,
 		)
