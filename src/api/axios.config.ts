ebbcf2773eb59729e64b67258ec439c6bb5f1a28
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-08 23:54:28 +0100

    refactor: Decreased axios timeout

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance, CreateAxiosDefaults } from 'axios';
 const axiosOptions: CreateAxiosDefaults = {
 	baseURL: import.meta.env.VITE_API_ROUTE, // Base URL for all requests
 	headers: { 'Content-Type': 'application/json' }, // Default header for all requests
-	timeout: 60000, // 60 seconds
+	timeout: 30000, // 30 seconds
 	timeoutErrorMessage: 'Request timed out', // Error message when request times out
 	withCredentials: true, // Enable sending and receiving cookies from the server
 };
