2febe818fcbc4e30b5a3da452fda51c66be813c3
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-11 16:02:36 +0100

    feat: Recipe card component

---
+++ src/components/Recipe/index.tsx
@@ -0,0 +1,69 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { RecipesContext } from '../../contextProviders/RecipesContext';
+import { IRecipe } from '../../types/types';
+import Button from '../Button';
+import PostUserActions from '../PostUserActions';
+import Reveal from '../Reveal';
+
+function getRevealAnimation(index: number | undefined) {
+	if (index === undefined) return 'slide-bottom';
+	return index % 2 === 0 ? 'slide-left' : 'slide-right';
+}
+
+const Recipe = ({ recipe, index }: { recipe: IRecipe; index?: number }) => {
+	const recipesContext = useContext(RecipesContext);
+
+	return (
+		<div className="rounded lg:w-[374px] w-full mb-6 lg:mb-0">
+			<Reveal animation={getRevealAnimation(index)} width="100%">
+				<div className="w-full h-full bg-zinc-100">
+					<img
+						className="w-full rounded-t lg:h-[200px] sm:h-[300px] h-[200px] object-cover object-center"
+						src={recipe.recipeImages[0].cloudinaryImage}
+						loading="lazy"
+						alt="Recipe Image"
+					/>
+
+					<div className="p-2 h-[154px] overflow-hidden">
+						<h2 className="text-2xl font-bold leading-7 tracking-tight text-gray-900 font-bellefair">
+							{recipe.title.trim()}
+						</h2>
+						<p className="my-2 font-normal leading-5 text-gray-700">
+							{recipe.description?.trim() ?? 'No description provided'}
+						</p>
+					</div>
+
+					<div className="flex flex-row py-3 mx-3">
+						<PostUserActions
+							type="recipe"
+							id={recipe.id}
+							likes={recipe.likes}
+							comments={recipe.comments}
+							isLiked={recipe.isLiked}
+							onLikeUpdate={(id, newLikes, isLiked) => {
+								recipe.isLiked = isLiked;
+								recipe.likes = newLikes;
+								recipesContext.updateLikes(id, newLikes, isLiked);
+							}}
+						/>
+
+						<div className="flex justify-end flex-grow">
+							<Link to={'/recipe/' + recipe.id}>
+								<Button
+									type="button"
+									id={'view-recipe-' + recipe.id}
+									className="px-3 py-1 text-white bg-zinc-800"
+								>
+									View Recipe
+								</Button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</Reveal>
+		</div>
+	);
+};
+
+export default Recipe;
