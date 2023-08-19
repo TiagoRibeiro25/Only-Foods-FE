aea5c1c2ff47786f62b6af6578942b3c9ba2ff3a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-19 23:55:30 +0100

    fix: Fixed double down arrow

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -40,22 +40,6 @@ const Select: React.FC<SelectProps> = ({
 						</option>
 					))}
 				</select>
-				<div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 pointer-events-none">
-					<svg
-						className="w-4 h-4"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M19 9l-7 7-7-7"
-						></path>
-					</svg>
-				</div>
 			</div>
 		</>
 	);
