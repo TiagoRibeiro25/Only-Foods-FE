60020cd80ca85bcc6d3963db1fb98e2dede19ec7
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-19 16:08:39 +0100

    feat: Delete recipe button (finished)

--- src/views/Recipe/components/DeleteRecipeButton/index.tsx
+++ src/views/Recipe/components/DeleteRecipeButton/index.tsx
@@ -1,9 +1,12 @@
 import { useContext, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useNavigate } from 'react-router-dom';
+import requests from '../../../../api/requests';
 import { Recipe } from '../../../../api/requests/recipes/getRecipe';
 import DeleteIcon from '../../../../assets/icons/delete.svg';
 import LoadingIcon from '../../../../assets/icons/loading.svg';
 import ConfirmActionModal from '../../../../components/ConfirmActionModal';
+import { RecipesContext } from '../../../../contextProviders/RecipesContext';
 import { UserContext } from '../../../../contextProviders/UserContext';
 
 interface DeleteRecipeButtonProps {
@@ -11,6 +14,9 @@ interface DeleteRecipeButtonProps {
 }
 
 const DeleteRecipeButton: React.FC<DeleteRecipeButtonProps> = ({ recipe }) => {
+	const navigate = useNavigate();
+
+	const recipesContext = useContext(RecipesContext);
 	const { loggedUser } = useContext(UserContext);
 
 	const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
@@ -20,9 +26,18 @@ const DeleteRecipeButton: React.FC<DeleteRecipeButtonProps> = ({ recipe }) => {
 		setShowDeleteModal(false);
 		setDeletingRecipe(true);
 
-		setTimeout(() => {
+		try {
+			const response = await requests.recipes.deleteRecipe(recipe.id);
+
+			if (response.data.success) {
+				recipesContext.deleteRecipe(recipe.id);
+				navigate('/recipes/all');
+			}
+		} catch (error) {
+			console.log('An error occurred while deleting the recipe: ', error);
+		} finally {
 			setDeletingRecipe(false);
-		}, 1000);
+		}
 	};
 
 	return (
@@ -45,7 +60,7 @@ const DeleteRecipeButton: React.FC<DeleteRecipeButtonProps> = ({ recipe }) => {
 			)}
 
 			<ConfirmActionModal
-				id="confirm-delete-feed"
+				id="confirm-delete-recipe-modal"
 				message="Are you sure you want to delete this recipe?"
 				onConfirm={deleteRecipe}
 				onCancel={() => setShowDeleteModal(false)}
