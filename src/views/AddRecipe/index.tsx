ff4df052c931c364c1f96be11b1143481d0f6c96
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-20 18:29:10 +0100

    feat: Create new recipe (finished)

--- src/views/AddRecipe/index.tsx
+++ src/views/AddRecipe/index.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import classNames from 'classnames';
+import { useMemo, useState } from 'react';
+import requests from '../../api/requests';
+import LoadingIcon from '../../assets/icons/loading.svg';
 import AddImages from '../../components/AddImages';
+import Button from '../../components/Button';
+import DynamicList, { Item } from '../../components/DynamicList';
 import Input from '../../components/Input';
 import Reveal from '../../components/Reveal';
 import Textarea from '../../components/Textarea';
@@ -9,6 +14,58 @@ const AddRecipe: React.FC = () => {
 	const [images, setImages] = useState<Base64Img[]>([]);
 	const [title, setTitle] = useState<string>('');
 	const [description, setDescription] = useState<string>('');
+	const [ingredients, setIngredients] = useState<Item[]>([{ id: '1', value: '' }]);
+	const [instructions, setInstructions] = useState<Item[]>([{ id: '1', value: '' }]);
+	const [notes, setNotes] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const [statusMSg, setStatusMSg] = useState<string>('');
+
+	const isButtonDisabled = useMemo(() => {
+		if (images.length === 0) return true;
+		if (title.trim() === '') return true;
+		if (ingredients.length === 1) return true;
+		if (instructions.length === 1) return true;
+		if (loading) return true;
+	}, [images.length, ingredients.length, instructions.length, loading, title]);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+		e.preventDefault();
+
+		setLoading(true);
+		setStatusMSg('');
+
+		try {
+			const response = await requests.recipes.addRecipe({
+				title,
+				description,
+				ingredients: ingredients.map(ingredient => ingredient.value).slice(0, -1),
+				instructions: instructions.map(instruction => instruction.value).slice(0, -1),
+				notes,
+				recipeImages: images,
+			});
+
+			if (response.data.success) {
+				setStatusMSg('Recipe successfully created!');
+
+				// Reset the form
+				setImages([]);
+				setTitle('');
+				setDescription('');
+				setIngredients([{ id: '1', value: '' }]);
+				setInstructions([{ id: '1', value: '' }]);
+				setNotes('');
+			} else {
+				setStatusMSg(response.data.message);
+			}
+		} catch (error) {
+			console.log('An error occurred while creating the recipe: ', error);
+			setStatusMSg(
+				'An error occurred while creating the recipe. Please try again later.',
+			);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	return (
 		<div className="flex flex-col items-center justify-center max-w-3xl mx-auto">
@@ -16,7 +73,8 @@ const AddRecipe: React.FC = () => {
 				<h1 className="w-full mt-3 text-3xl font-bellefair">Create a New Recipe</h1>
 			</Reveal>
 
-			<form className="w-full my-10">
+			<form className="w-full my-10" onSubmit={handleSubmit}>
+				{/* Images */}
 				<Reveal width="100%" animation="slide-right" delay={0.2}>
 					<h2 className="mb-3 text-2xl font-bellefair">Images</h2>
 				</Reveal>
@@ -24,7 +82,8 @@ const AddRecipe: React.FC = () => {
 					<AddImages images={images} setImages={setImages} />
 				</Reveal>
 
-				<Reveal width="100%" animation="slide-bottom" delay={0.2}>
+				{/* Title */}
+				<Reveal width="100%" animation="slide-right" delay={0.2}>
 					<Input
 						className="mt-12"
 						type="text"
@@ -38,6 +97,7 @@ const AddRecipe: React.FC = () => {
 					/>
 				</Reveal>
 
+				{/* Description */}
 				<Reveal width="100%" animation="slide-right" delay={0.2}>
 					<h2 className="mt-12 mb-3 text-2xl font-bellefair">Description</h2>
 				</Reveal>
@@ -53,6 +113,71 @@ const AddRecipe: React.FC = () => {
 					>
 						{description}
 					</Textarea>
+				</Reveal>
+
+				{/* Ingredients */}
+				<Reveal width="100%" animation="slide-right" delay={0.2}>
+					<h2 className="mt-12 mb-3 text-2xl font-bellefair">Ingredients</h2>
+				</Reveal>
+				<DynamicList
+					type="Ingredient"
+					list={ingredients}
+					updateList={updatedList => setIngredients(updatedList)}
+				/>
+
+				{/* Instructions */}
+				<Reveal width="100%" animation="slide-right" delay={0.2}>
+					<h2 className="mt-12 mb-3 text-2xl font-bellefair">Instructions</h2>
+				</Reveal>
+				<DynamicList
+					type="Instruction"
+					list={instructions}
+					updateList={updatedList => setInstructions(updatedList)}
+				/>
+
+				{/* Notes */}
+				<Reveal width="100%" animation="slide-right" delay={0.2}>
+					<h2 className="mt-12 mb-3 text-2xl font-bellefair">Notes</h2>
+				</Reveal>
+				<Reveal width="100%" animation="slide-bottom" delay={0.2}>
+					<Textarea
+						className="p-3 text-sm border-gray-300 rounded-md focus:border-gray-300"
+						name="notes"
+						id="notes"
+						placeholder="Enter some notes about your recipe (optional)"
+						autoComplete="New Recipe Notes"
+						resizable
+						onChange={e => setNotes(e)}
+					>
+						{notes}
+					</Textarea>
+				</Reveal>
+
+				{/* Status Message */}
+				{statusMSg !== '' && (
+					<Reveal width="100%" delay={0.2}>
+						<p className="mt-6 text-sm text-center text-gray-500">{statusMSg}</p>
+					</Reveal>
+				)}
+
+				{/* Submit Button */}
+				<Reveal width="100%" animation="slide-bottom" delay={0.2}>
+					<div className="flex items-center justify-center w-full mt-6">
+						<Button
+							type="submit"
+							id="submit-create-recipe-form-button"
+							className={classNames(
+								'text-white bg-zinc-800 py-1.5',
+								loading ? 'px-11' : 'px-16',
+							)}
+							icon={loading ? LoadingIcon : ''}
+							iconAlt="Loading Icon"
+							iconAnimation="spin"
+							disabled={isButtonDisabled}
+						>
+							{loading ? 'Creating Recipe' : 'Create Recipe'}
+						</Button>
+					</div>
 				</Reveal>
 			</form>
 		</div>
