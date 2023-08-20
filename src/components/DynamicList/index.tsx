e1e7a15076af0320ec4cea682ccfb41c76a0a4a1
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-20 17:31:55 +0100

    feat: Dynamic List component
    Component that generates a dynamic list of items. It takes in a type, list, and updateList as props. The type defines the type of items, the list holds the items' data, and the updateList function is used to modify the list. The component renders an editable list where each item can be modified or removed. Empty items are automatically managed: when an item is edited, empty items are removed, and a new empty item is added if the last item isn't empty.

---
+++ src/components/DynamicList/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Input from '../Input';
+import Reveal from '../Reveal';
+
+export interface Item {
+	id: string;
+	value: string;
+}
+
+interface DynamicListProps {
+	type: string;
+	list: Item[];
+	updateList: (list: Item[]) => void;
+}
+
+const DynamicList: React.FC<DynamicListProps> = ({ type, list, updateList }) => {
+	const handleItemChange = (id: string, newValue: string) => {
+		// Update the list with the new value
+		const newList = list.map(listItem => {
+			return listItem.id === id ? { id: listItem.id, value: newValue } : listItem;
+		});
+
+		// Check if there are any empty items in the list and remove them
+		const updatedList = newList.filter(item => item.value.trim() !== '');
+
+		// If the last item in the list is not empty, add a new empty item
+		if (updatedList[updatedList.length - 1]?.value.trim() !== '') {
+			updatedList.push({
+				id: (parseInt(updatedList[updatedList.length - 1]?.id || '0') + 1).toString(),
+				value: '',
+			});
+		}
+
+		updateList(updatedList);
+	};
+
+	return (
+		<ul className="mt-3 space-y-3 text-gray-500 list-disc list-inside">
+			{list.map(item => (
+				<Reveal key={item.id} width="100%" animation="slide-bottom" delay={0.05}>
+					<Input
+						id={'dynamic-list-item-' + item.id}
+						type="text"
+						className="mt-2"
+						name={type}
+						placeholder={type + ' ' + (list.indexOf(item) + 1)}
+						value={item.value}
+						onChange={(newValue: string) => handleItemChange(item.id, newValue)}
+					/>
+				</Reveal>
+			))}
+		</ul>
+	);
+};
+
+export default DynamicList;
