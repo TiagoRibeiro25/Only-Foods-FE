10ed1693c65bb91d005fef48dae459c0f9f63ae7
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-21 18:59:15 +0100

    refactor: Added new API calls to the requests object

--- src/api/requests/users/index.ts
+++ src/api/requests/users/index.ts
@@ -1,8 +1,21 @@
 import forgotPassword from './forgotPassword';
 import getLoggedUser from './getLoggedUser';
+import getUser from './getUser';
+import handleBlockUser from './handleBlockUser';
+import handleFollow from './handleFollow';
 import login from './login';
 import logout from './logout';
 import register from './register';
 import resetPassword from './resetPassword';
 
-export default { forgotPassword, resetPassword, register, login, getLoggedUser, logout };
+export default {
+	forgotPassword,
+	resetPassword,
+	register,
+	login,
+	getLoggedUser,
+	logout,
+	getUser,
+	handleFollow,
+	handleBlockUser,
+};
