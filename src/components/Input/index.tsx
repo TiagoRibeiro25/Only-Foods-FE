720e8797d44b63b26cd8bc33b0150daac89d2005
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-24 19:52:40 +0100

    refactor: Improved Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,16 +2,16 @@ import classNames from 'classnames';
 import { useState } from 'react';
 
 interface InputProps {
-	placeholder: string;
+	placeholder?: string;
 	className?: string;
-	type: 'text' | 'password' | 'email' | 'number' | 'tel';
+	type: 'text' | 'password' | 'email' | 'number' | 'tel' | 'url' | 'search';
 	name: string;
 	id?: string;
 	value: string;
 	required?: boolean;
 	disabled?: boolean;
 	autoComplete?: string;
-	onChange: (value: string) => void;
+	onChange?: (value: string) => void;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -29,6 +29,7 @@ const Input: React.FC<InputProps> = ({
 	const [inputValue, setInputValue] = useState<string>(value ?? '');
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		if (!onChange) return;
 		setInputValue(event.target.value);
 		onChange(event.target.value);
 	};
@@ -44,7 +45,7 @@ const Input: React.FC<InputProps> = ({
 				type={type}
 				name={name}
 				{...(id ? { id } : {})}
-				className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-b-gray-900 peer"
+				className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-b-gray-900 disabled:opacity-50 peer"
 				placeholder=" "
 				autoComplete={autoComplete ?? 'off'}
 				required={required ?? false}
@@ -54,9 +55,12 @@ const Input: React.FC<InputProps> = ({
 			/>
 			<label
 				htmlFor={id}
-				className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+				className={classNames(
+					'peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6',
+					{ 'opacity-90': disabled },
+				)}
 			>
-				{placeholder}
+				{placeholder ?? 'Enter your text here'}
 			</label>
 		</div>
 	);
